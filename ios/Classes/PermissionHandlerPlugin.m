afb1db3658bf17172e7e0e827cb990fc5946c803
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-02-15 21:34:15 +0200

    migrate Swift and Kotlin to ObjC and Java

--- ios/Classes/PermissionHandlerPlugin.m
+++ ios/Classes/PermissionHandlerPlugin.m
@@ -1,8 +1,60 @@
 #import "PermissionHandlerPlugin.h"
-#import <permission_handler/permission_handler-Swift.h>
 
-@implementation PermissionHandlerPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftPermissionHandlerPlugin registerWithRegistrar:registrar];
+
+@implementation PermissionHandlerPlugin {
+    PermissionManager *_Nonnull _permissionManager;
+    _Nullable FlutterResult _methodResult;
 }
+
+- (instancetype)initWithPermissionManager:(PermissionManager *)permissionManager {
+    self = [super init];
+    if (self) {
+        _permissionManager = permissionManager;
+    }
+    
+    return self;
+}
+
++ (void)registerWithRegistrar:(NSObject <FlutterPluginRegistrar> *)registrar {
+    FlutterMethodChannel *channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"flutter.baseflow.com/permissions/methods"
+                                     binaryMessenger:[registrar messenger]];
+    PermissionManager *permissionManager = [[PermissionManager alloc] initWithStrategyInstances];
+    PermissionHandlerPlugin *instance = [[PermissionHandlerPlugin alloc] initWithPermissionManager:permissionManager];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if ([@"checkPermissionStatus" isEqualToString:call.method]) {
+        PermissionGroup permission = [Codec decodePermissionGroupFrom:call.arguments];
+        [PermissionManager checkPermissionStatus:permission result:result];
+    } else if ([@"checkServiceStatus" isEqualToString:call.method]) {
+        PermissionGroup permission = [Codec decodePermissionGroupFrom:call.arguments];
+        [PermissionManager checkServiceStatus:permission result:result];
+    } else if ([@"requestPermissions" isEqualToString:call.method]) {
+        if (_methodResult != nil) {
+            result([FlutterError errorWithCode:@"ERROR_ALREADY_REQUESTING_PERMISSIONS" message:@"A request for permissions is already running, please wait for it to finish before doing another request (note that you can request multiple permissions at the same time)." details:nil]);
+        }
+        
+        _methodResult = result;
+        NSArray *permissions = [Codec decodePermissionGroupsFrom:call.arguments];
+        
+        [_permissionManager
+         requestPermissions:permissions completion:^(NSDictionary *permissionRequestResults) {
+             if (self->_methodResult != nil) {
+                 self->_methodResult(permissionRequestResults);
+             }
+             
+             self->_methodResult = nil;
+         }];
+        
+    } else if ([@"shouldShowRequestPermissionRationale" isEqualToString:call.method]) {
+        result(@false);
+    } else if ([@"openAppSettings" isEqualToString:call.method]) {
+        [PermissionManager openAppSettings:result];
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end
--- ios/permission_handler.podspec
+++ ios/permission_handler.podspec
@@ -3,20 +3,19 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'permission_handler'
-  s.version          = '2.2.0'
-  s.summary          = 'Permission plugin for Flutter.'
+  s.version          = '0.0.1'
+  s.summary          = 'A new Flutter project.'
   s.description      = <<-DESC
-Permission plugin for Flutter. This plugin provides a cross-platform (iOS, Android) API to request and check permissions.
+A new Flutter project.
                        DESC
-  s.homepage         = 'https://github.com/baseflowit/flutter-permission-handler'
+  s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Baseflow' => 'hello@baseflow.com' }
+  s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
-  s.swift_version = '4.2'
+
   s.ios.deployment_target = '8.0'
-  s.pod_target_xcconfig = { 'SWIFT_VERSION' => '4.2' }
 end
 
