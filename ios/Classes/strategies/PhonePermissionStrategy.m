d06ed39e82841a8fcbac407c8ce146c7b85ce91d
Sebastian Roth <sebastian.roth@gmail.com>
2019-05-20 20:04:00 +0100

    Adds ServiceStatus (phone) inquiry for iOS

---
+++ ios/Classes/strategies/PhonePermissionStrategy.m
@@ -0,0 +1,58 @@
+//
+//  PhonePermissionStrategy.m
+//  permission_handler
+//
+//  Created by Sebastian Roth on 5/20/19.
+//
+
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
+
+#import "PhonePermissionStrategy.h"
+
+@implementation PhonePermissionStrategy
+
+- (PermissionStatus)checkPermissionStatus:(PermissionGroup)permission {
+    return PermissionStatusUnknown;
+}
+
+- (ServiceStatus)checkServiceStatus:(PermissionGroup)permission {
+    // https://stackoverflow.com/a/5095058
+    if (![[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel://"]]) {
+        return ServiceStatusNotApplicable;
+    }
+
+    return [self canDevicePlaceAPhoneCall] ? ServiceStatusEnabled : ServiceStatusDisabled;
+}
+
+- (void)requestPermission:(PermissionGroup)permission completionHandler:(PermissionStatusHandler)completionHandler {
+    completionHandler(PermissionStatusUnknown);
+}
+
+
+// https://stackoverflow.com/a/11595365
+-(bool) canDevicePlaceAPhoneCall {
+    /*
+     * Returns YES if the device can place a phone call
+     */
+
+    // Check if the device can place a phone call
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel://"]]) {
+        // Device supports phone calls, lets confirm it can place one right now
+        CTTelephonyNetworkInfo *netInfo = [[CTTelephonyNetworkInfo alloc] init];
+        CTCarrier *carrier = [netInfo subscriberCellularProvider];
+        NSString *mnc = [carrier mobileNetworkCode];
+        if (([mnc length] == 0) || ([mnc isEqualToString:@"65535"])) {
+            // Device cannot place a call at this time.  SIM might be removed.
+            return NO;
+        } else {
+            // Device can place a phone call
+            return YES;
+        }
+    } else {
+        // Device does not support phone calls
+        return  NO;
+    }
+}
+
+@end
