4026813d6aed8d4942bb7e9d5d0fc9ada2be0ae0
飞龙 <89000073+jinchengwu-ola@users.noreply.github.com>
2023-09-13 15:59:17 +0800

    Reply already submitted in Android 13 while user request permissions (#1147)
    * Reply already submitted in Android 13 while user request permissions(eg.
    post_notification, locations)
    
    * Update permission_handler_android/android/src/main/java/com/baseflow/permissionhandler/PermissionManager.java
    
    Co-authored-by: Jeroen Weener <JeroenWeener@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jeroen Weener <JeroenWeener@users.noreply.github.com>

--- permission_handler_android/android/src/main/java/com/baseflow/permissionhandler/PermissionManager.java
+++ permission_handler_android/android/src/main/java/com/baseflow/permissionhandler/PermissionManager.java
@@ -161,6 +161,11 @@ final class PermissionManager implements PluginRegistry.ActivityResultListener, 
             return false;
         }
 
+        if (permissions.length == 0 && grantResults.length == 0) {
+            Log.w(PermissionConstants.LOG_TAG, "onRequestPermissionsResult is called without results. This is probably caused by interfering request codes. If you see this error, please file an issue in flutter-permission-handler, including a list of plugins used by this application: https://github.com/Baseflow/flutter-permission-handler/issues");
+            return false;
+        }
+
         for (int i = 0; i < permissions.length; i++) {
             final String permissionName = permissions[i];
 
