2eefb819343762483158366750cfba6338fede5c
Jeroen Weener <JeroenWeener@users.noreply.github.com>
2023-11-01 15:56:32 +0100

    Implement `Settings` mirror

---
+++ permission_handler_android/lib/src/android_object_mirrors/settings.dart
@@ -0,0 +1,12 @@
+/// The Settings provider contains global system-level device preferences.
+///
+/// See https://developer.android.com/reference/android/provider/Settings.
+class Settings {
+  const Settings._();
+
+  /// Activity Action: Show screen of details about a particular application.
+  ///
+  /// Constant Value: "android.settings.APPLICATION_DETAILS_SETTINGS".
+  static const actionApplicationDetailsSettings =
+      'android.settings.APPLICATION_DETAILS_SETTINGS';
+}
