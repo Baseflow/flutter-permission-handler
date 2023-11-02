388dfacaa9ff3b4471b09273a368c4bcce60a024
Alex Melnyk <30177329+alex-melnyk@users.noreply.github.com>
2023-11-02 11:53:29 +0200

    remove template code

--- permission_handler_apple/lib/permission_handler_apple.dart
+++
@@ -1,7 +0,0 @@
-import 'permission_handler_apple_platform_interface.dart';
-
-class PermissionHandlerApple {
-  Future<String?> getPlatformVersion() {
-    return PermissionHandlerApplePlatform.instance.getPlatformVersion();
-  }
-}
