e27bfc2e5cef8e449e8c456615a415a9ff06642a
Alex Melnyk <30177329+alex-melnyk@users.noreply.github.com>
2024-01-25 18:38:09 +0200

    macos example project updated

--- permission_handler/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ permission_handler/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import permission_handler_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PermissionHandlerPlugin.register(with: registry.registrar(forPlugin: "PermissionHandlerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
